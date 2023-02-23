7742e3b1f6cf6f12083c2334b807bb13d42e8c0f
0x4448 <86135470+0x4448@users.noreply.github.com>
2023-02-23 06:46:47 +0000

    Add initial project template

---
+++ Assets/Scripts/Core/Startup.cs
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: Apache-2.0
+// https://github.com/0x4448/unity-samples/blob/main/LICENSE
+
+using UnityEngine;
+
+namespace UnitySamples
+{
+    public class Startup : MonoBehaviour
+    {
+
+    }
+}
